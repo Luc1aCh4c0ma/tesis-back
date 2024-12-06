f126be322f5d0cd977f0e5ffd6fc5f054c400181
Luc1aCh4c0ma <132414863+Luc1aCh4c0ma@users.noreply.github.com>
2024-12-06 13:55:29 -0300

    changes_db

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,11 +15,11 @@ import { TicketModule } from './ticket/ticket.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123456',
-      database: 'lyr',
+      host: process.env.DB_HOST,
+      port:+ process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true, // ⚠️ Usa solo en desarrollo
     }),
